d1202d61e1ba5da88645c76d96d22642bf94e1b4
zeloff <zelofflab@yandex.ru>
2018-12-31 17:59:10 +0300

    :sparkles:Add store creation

--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,10 @@
-// store initialization
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './modules'
+
+// eslint-disable-next-line
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
+
+export default store
