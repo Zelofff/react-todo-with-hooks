0e1a8e9192f51572950ac6ec83b3a034246887fd
zelofff <zelofflab@yandex.ru>
2019-06-14 02:06:54 +0300

    :tada: Init project configuration

---
+++ config/webpack.base.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+      '@components': path.resolve(__dirname, '../src/components/')
+    },
+    extensions: ['.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+}
---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { hot } from 'react-hot-loader/root'
+
+export const App = hot(() => (
+  <>
+    <header>
+      <h1>Hello world</h1>
+    </header>
+    <main>hello world</main>
+  </>
+))
