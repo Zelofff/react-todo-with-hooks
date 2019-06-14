7733e67b652addeb2d233878a6bdb92bac6be229
zelofff <zelofflab@yandex.ru>
2019-06-14 14:02:43 +0300

    :lipstick: Add global styles

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { hot } from 'react-hot-loader/root'
+import './global-styles'
 
 import { TodosPage } from '@features/todo'
 
 const AppContainer = () => {
-  return <TodosPage />
+  return <TodosPage className={global} />
 }
 
 export const App = hot(AppContainer)
---
+++ src/global-styles.js
@@ -0,0 +1,10 @@
+import { css } from 'linaria'
+
+export const globals = css`
+  :global() {
+    body {
+      margin: 0;
+      padding: 0;
+    }
+  }
+`
