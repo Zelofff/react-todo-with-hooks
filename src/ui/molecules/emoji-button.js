14af6c57face08175a2e071569324e16428c1923
zelofff <zelofflab@yandex.ru>
2019-06-16 15:40:23 +0300

    :lipstick: Fix emoji button outline

--- src/ui/molecules/emoji-button.js
+++ src/ui/molecules/emoji-button.js
@@ -1,18 +1,10 @@
 import { styled } from 'linaria/react'
 
 export const EmojiButton = styled.button`
-  border: none;
-  border-radius: 50%;
-  cursor: pointer;
   font-size: 2rem;
   line-height: 2rem;
-  width: 2rem;
-  height: 2rem;
   padding: 5px;
-  outline: none;
-  box-sizing: content-box;
-
-  :focus {
-    border: 1px solid black;
-  }
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
 `
