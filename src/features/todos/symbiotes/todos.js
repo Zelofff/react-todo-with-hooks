71af182116654925ec939098430e58e2bd5ed510
zelofff <zelofflab@yandex.ru>
2019-06-22 01:51:47 +0300

    :art: Update todos symbiote

--- src/features/todos/symbiotes/todos.js
+++ src/features/todos/symbiotes/todos.js
@@ -4,21 +4,14 @@ import nanoid from 'nanoid'
 const initialState = []
 
 const symbiotes = {
-  add: (state, text) => [
-    ...state,
-    {
-      id: nanoid(10),
-      completed: false,
-      text
-    }
-  ],
+  add: (state, text) => [...state, { id: nanoid(10), completed: false, text }],
   remove: (state, id) => state.filter(todo => todo.id !== id),
+  edit: (state, id, text) =>
+    state.map(todo => (todo.id === id ? { ...todo, text } : todo)),
   toggle: (state, id) =>
     state.map(todo =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    ),
-  edit: (state, id, text) =>
-    state.map(todo => (todo.id === id ? { ...todo, text } : todo))
+    )
 }
 
 export const { actions, reducer } = createSymbiote(
