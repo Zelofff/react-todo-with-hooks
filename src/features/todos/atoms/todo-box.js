d0b436cb63a34d5b31cf79315e9c21c38a2c8a9a
zelofff <zelofflab@yandex.ru>
2019-06-23 02:52:53 +0300

    :lipstick: Update todo styles

--- src/features/todos/atoms/todo-box.js
+++ src/features/todos/atoms/todo-box.js
@@ -4,9 +4,12 @@ export const TodoBox = styled.li`
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
-  background-color: gray;
-  border-top: 1px solid #000;
+  background-color: #fff;
+  border-radius: 5rem;
+  box-shadow: 0px 6px 20px -5px rgba(0, 0, 0, 0.5);
+  margin-bottom: 1.2rem;
   padding: 1rem;
   width: 100%;
+  max-height: 4.5rem;
   box-sizing: border-box;
 `
--- src/ui/atoms/input.js
+++ src/ui/atoms/input.js
@@ -1,3 +1,11 @@
 import { styled } from 'linaria/react'
 
-export const Input = styled.input``
+export const Input = styled.input`
+  font-family: 'Helvetica', sans-serif;
+  font-size: 1.4rem;
+  border: 1px solid #000;
+  border-radius: 0.2rem;
+  padding: 0.5rem;
+  width: 100%;
+  box-sizing: border-box;
+`
