9a6935f704cecdf51e9c300db2754067d84ddcae
zelofff <zelofflab@yandex.ru>
2019-06-24 10:12:08 +0300

    :lipstick: Fix todo text styles

--- src/features/todos/atoms/todo-text.js
+++ src/features/todos/atoms/todo-text.js
@@ -2,7 +2,6 @@ import { styled } from 'linaria/react'
 import { Text } from '@ui'
 
 export const TodoText = styled(Text)`
-  min-width: 6rem;
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
