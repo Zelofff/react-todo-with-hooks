2ebcfcfce455a58f586ade29bfd1e4c585c6c2f8
zelofff <zelofflab@yandex.ru>
2019-07-02 21:26:13 +0300

    :lipstick: Change size of header

--- src/features/common/atoms/header-box.js
+++ src/features/common/atoms/header-box.js
@@ -5,7 +5,7 @@ export const HeaderBox = styled.div`
   flex-flow: row nowrap;
   justify-content: space-between;
   align-items: center;
-  padding: 15px 30px;
+  padding: 25px 30px;
   background-color: var(--color-primary);
   color: var(--color-on-primary);
   box-shadow: 0px 5px 20px -5px var(--color-shadow);
