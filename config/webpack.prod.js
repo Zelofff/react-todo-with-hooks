f1b5956b12654708a620cd5dc1c94039672598bb
zelofff <zelofflab@yandex.ru>
2019-09-22 18:36:13 +0300

    :sparkles: Add optimize-css-assets-webpack-plugin

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,14 @@
 const base = require('./webpack.base')
 const merge = require('webpack-merge')
 const CompressionPlugin = require('compression-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const mode = 'production'
 
 module.exports = merge(base(mode), {
   mode,
-  plugins: [new CompressionPlugin({ test: /\.js(\?.*)?$/i })]
+  plugins: [
+    new CompressionPlugin({ test: /\.js(\?.*)?$/i }),
+    new OptimizeCssAssetsPlugin()
+  ]
 })
